a843d20527f1178569f5a7774703e4be9565b12d
Raphael Joer <ra.joer@gmail.com>
2020-04-23 13:12:34 -0300

    testes aprovados

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React from "react";
-
-import "./styles.css";
-
-function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
-
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
-
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
-}
-
-export default App;
+import React, { useState, useEffect } from "react";
+    
+    import "./styles.css";
+    import api from "./services/api";
+    
+    function App() {
+    
+      const [repositories, setRepositories] = useState([]);
+    
+      useEffect(() => {
+        api.get('repositories').then(response => {
+          setRepositories(response.data);
+        });
+      }, []);
+    
+      async function handleAddRepository() {
+    
+        const response = await api.post('repositories', {
+          title: "React JS",
+          url: "https://github.com/Rocketseat/joer",
+          techs: ["Node.js","Express","TypeScript"]
+        })
+    
+        setRepositories([ ...repositories, response.data ]);
+        console.log(response.data);
+    
+      };
+    
+      async function handleRemoveRepository(id) {
+        
+        await api.delete(`repositories/${id}`);
+    
+        const newRepositories = repositories.filter(
+          repository => repository.id !== id
+        );
+    
+        setRepositories(newRepositories);
+    
+      };
+    
+      return (
+        <div>
+    
+          <ul data-testid="repository-list">
+              
+              {repositories.map(repository => (
+                <li key={repository.id}>
+                  {repository.title}
+                  <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+                </li>
+              ))}
+    
+          </ul>
+    
+          <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+      );
+    }
+    
+    export default App;
